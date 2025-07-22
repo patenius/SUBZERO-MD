4a451a5ba7474fecb6e6e22c4c93c7da05da209e
patenius <patheniuspatenius@gmail.com>
2025-07-22 20:46:33 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: ""SUBZERO-MD;;;nItXHCqZ#dGhDWxDP0h03fDvfeO9iVdXEjM6B7Zhn748t2jVXxR4"*"
+  SESSION_ID: "SUBZERO-MD;;;nItXHCqZ#dGhDWxDP0h03fDvfeO9iVdXEjM6B7Zhn748t2jVXxR4"
   OWNER_NUMBER: "254114498349", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
