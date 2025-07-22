ec78665cec0a803b5b011d83e4fa586b1f1b9900
patenius <patheniuspatenius@gmail.com>
2025-07-22 19:36:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: ""SUBZERO-MD;;;nItXHCqZ#dGhDWxDP0h03fDvfeO9iVdXEjM6B7Zhn748t2jVXxR4"*"
+  OWNER_NUMBER: "254114498349", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
